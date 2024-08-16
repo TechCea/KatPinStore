750badcc519b9961dc3c27560885d8aa3a4fd615
BJaviersito <losthyghost@gmail.com>
2024-08-15 21:00:39 -0600

    responsive

--- JS/main.js
+++ JS/main.js
@@ -70,228 +70,170 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-// PRODUCTOS
 const productos = [
     {
         id: "PIN-01",
-        titulo: "Pin de Gato",
+        titulo: "Pin de Gato Asesino",
         imagen: "./IMG/gatoasesinoP.jpg",
         imagenesCarrusel: ["./IMG/gatoasesinoP.jpg", "./IMG/gatoasesinomano.jpeg"],
         categoria: { nombre: "Gatos", id: "Gatos" },
         precio: 1.50,
-        descripcion: "Pin de gato adorable."
+        descripcion: "Pin de un gato con una expresión maliciosa, ideal para los amantes de los felinos con carácter."
     },
     {
         id: "PIN-02",
-        titulo: "Pin de Gatito ",
+        titulo: "Pin de Gatito Trash",
         imagen: "./IMG/gatotrash.jpg",
         imagenesCarrusel: ["./IMG/gatotrash.jpg", "./IMG/gatitotrashman.jpg"],
         categoria: { nombre: "Gatos", id: "Gatos" },
         precio: 3.50,
-        descripcion: "Llavero de Articuno, el legendario Pokémon."
+        descripcion: "Pin de un gatito rebelde en un estilo trash, perfecto para aquellos que aprecian el arte urbano."
     },
     {
         id: "PIN-03",
         titulo: "Pin de Capibara",
         imagen: "./IMG/capibara1.jpg",
         imagenesCarrusel: ["./IMG/capibara1.jpg", "./IMG/CapibaraMano.jpg"],
-        categoria: { nombre: "Valorant", id: "valorant" },
+        categoria: { nombre: "Animales", id: "animales" },
         precio: 3.50,
-        descripcion: "Llavero de Astra de Valorant."
+        descripcion: "Pin de una capibara relajada, el roedor más grande y amigable del mundo, en un diseño adorable."
     },
     {
         id: "PIN-04",
         titulo: "Pin de Pikachu",
         imagen: "./IMG/pikachu.jpg",
         imagenesCarrusel: ["./IMG/pikachu.jpg", "./IMG/pikachumano.jpg"],
-        categoria: {
-            nombre: "Pokemon",
-            id: "Pokemon"
-        },
+        categoria: { nombre: "Pokemon", id: "Pokemon" },
         precio: 3.50,
-         descripcion: "Pin de gato adorable."
+        descripcion: "Pin del icónico Pikachu, el Pokémon más querido de todos, con una expresión juguetona."
     },
     {
         id: "PIN-05",
         titulo: "Pin de Gengar",
         imagen: "./IMG/gengar.jpg",
         imagenesCarrusel: ["./IMG/gengar.jpg","./IMG/gengarmano.jpg", "./IMG/gengarmano1.jpg"],
-        categoria: {
-            nombre: "Pokemon",
-            id: "pokemon"
-        },
+        categoria: { nombre: "Pokemon", id: "Pokemon" },
         precio: 3.50,
-        descripcion: "Pin de gato adorable."
+        descripcion: "Pin de Gengar, el Pokémon fantasma, con una sonrisa traviesa que encanta a todos."
     },
     {
         id: "PIN-06",
         titulo: "Pin de Astronauta",
         imagen: "./IMG/astronauta.jpg",
         imagenesCarrusel: ["./IMG/astronauta.jpg", "./IMG/astronauta1.jpg"],
-        categoria: {
-            nombre: "Accion",
-            id: "accion"
-        },
+        categoria: { nombre: "Espacio", id: "espacio" },
         precio: 3.50,
-        descripcion: "Pin de gato adorable."
+        descripcion: "Pin de un astronauta flotando en el espacio, perfecto para los aficionados de la exploración espacial."
     },
     {
         id: "PIN-07",
         titulo: "Pin de Gato Fantasma",
         imagen: "./IMG/fantasmagato1.jpg",
         imagenesCarrusel: ["./IMG/fantasmagato1.jpg", "./IMG/fantasmagato2.jpg"],
-        categoria: {
-            nombre: "Pokemon",
-            id: "pokemon"
-        },
+        categoria: { nombre: "Gatos", id: "Gatos" },
         precio: 3.50,
-        descripcion: "Pin de gato adorable."
+        descripcion: "Pin de un gato con un disfraz de fantasma, ideal para los amantes de lo paranormal y lo felino."
     },
     {
         id: "PIN-08",
-        titulo: "Pin de Gatito",
+        titulo: "Pin de Gatito Maceta",
         imagen: "./IMG/gattito.jpg",
         imagenesCarrusel: ["./IMG/gattito3.jpg", "./IMG/gattito2.jpg"],
-        categoria: {
-            nombre: "Accion",
-            id: "accion"
-        },
+        categoria: { nombre: "Gatos", id: "Gatos" },
         precio: 3.50,
-        descripcion: "Pin de gato adorable."
+        descripcion: "Pin de un gatito que emerge de una maceta, combinando la naturaleza con el amor por los gatos."
     },
     {
         id: "PIN-09",
-        titulo: "Pin de Gatito",
+        titulo: "Pin de Gatito Atrapado",
         imagen: "./IMG/gattitto.jpg",
         imagenesCarrusel: ["./IMG/gattitto.jpg", "./IMG/gattitto2.jpg"],
-        categoria: {
-            nombre: "Accion",
-            id: "accion"
-        },
+        categoria: { nombre: "Gatos", id: "Gatos" },
         precio: 3.50,
-        descripcion: "Pin de gato adorable."
+        descripcion: "Pin de un gatito que parece atrapado en una situación cómica, ideal para los que disfrutan del humor felino."
     },
     {
-      id: "PIN-10",
-      titulo: "Pin de Bob Esponja",
-      imagen: "./IMG/bob.jpg",
-      imagenesCarrusel: ["./IMG/bob.jpg", "./IMG/bob1.jpg"],
-      categoria: {
-          nombre: "Accion",
-          id: "accion"
-      },
-      precio: 3.50,
-      descripcion: "Pin de gato adorable."
-     },
-     {
-      id: "PIN-11",
-      titulo: "Pin de Nintendo",
-      imagen: "./IMG/nintendo.jpg",
-      imagenesCarrusel: ["./IMG/nintendo.jpg", "./IMG/nintendomano.jpg"],
-      categoria: {
-          nombre: "Anime",
-          id: "anime"
-      },
-      precio: 3.50,
-      descripcion: "Pin de gato adorable."
-      },
-      {
-          id: "PIN-12",
-          titulo: "Pin de Ganyu",
-          imagen: "./IMG/genshi.jpg",
-          imagenesCarrusel: ["./IMG/genshi.jpg", "./IMG/genshi1.jpg"],
-          categoria: {
-              nombre: "Genshin Impact",
-              id: "Genshin Impact"
-          },
-          precio: 7.50,
-          descripcion: "Pin de gato adorable."
-      },
-      {
-          id: "PIN-13",
-          titulo: "Pin de Xiao",
-          imagen: "./IMG/genshin.jpg",
-          imagenesCarrusel: ["./IMG/genshin.jpg", "./IMG/genshin2.jpg"],
-          categoria: {
-              nombre: "Genshin Impact",
-              id: "Genshin Impact"
-          },
-          precio: 6.50,
-          descripcion: "Pin de gato adorable."
-      },
-      {
-          id: "PIN14",
-          titulo: "Pin de Astronauta",
-          imagen: "./IMG/starastro.jpg",
-          imagenesCarrusel: ["./IMG/starastro.jpg", "./IMG/starastro1.jpg"],
-          categoria: {
-              nombre: "South-Park",
-              id: "south-park"
-          },
-          precio: 3.50,
-          descripcion: "Pin de gato adorable."
-      },
-      {
-          id: "PIN-15",
-          titulo: "Pin de Capibara Cumpleañero",
-          imagen: "./IMG/capibaracum.jpg",
-          imagenesCarrusel: ["./IMG/capibaracum.jpg", "./IMG/capibaracum1.jpg"],
-          categoria: {
-              nombre: "Valorant",
-              id: "valorant"
-          },
-          precio: 3.50,
-          descripcion: "Pin de gato adorable."
-      },
-      {
-          id: "PIN-16",
-          titulo: "Pin de Gravity Falls",
-          imagen: "./IMG/book1.jpg",
-          imagenesCarrusel: ["./IMG/book1.jpg", "./IMG/book.jpg"],
-          categoria: {
-              nombre: "Accion",
-              id: "accion"
-          },
-          precio: 3.50,
-          descripcion: "Pin de gato adorable."
-      },
-      {
-          id: "PIN-17",
-          titulo: "Pin de Gatito",
-          imagen: "./IMG/Gatomaceta.jpg",
-          imagenesCarrusel: ["./IMG/Gatomaceta.jpg", "./IMG/Gatomaceta1.jpg"],
-          categoria: {
-              nombre: "Valorant",
-              id: "valorant"
-          },
-          precio: 3.00,
-          descripcion: "Pin de gato adorable."
-      },
-      {
-          id: "PIN-18",
-          titulo: "Pin de Fantasma",
-          imagen: "./IMG/fantasmabook.jpg",
-          imagenesCarrusel: ["./IMG/fantasmabook.jpg", "./IMG/fantasmabook1.jpg"],
-          categoria: {
-              nombre: "Pokemon",
-              id: "pokemon"
-          },
-          precio: 3.50,
-          descripcion: "Pin de gato adorable."
-      }
-       /*{
+        id: "PIN-10",
+        titulo: "Pin de Bob Esponja",
+        imagen: "./IMG/bob.jpg",
+        imagenesCarrusel: ["./IMG/bob.jpg", "./IMG/bob1.jpg"],
+        categoria: { nombre: "Series Animadas", id: "series-animadas" },
+        precio: 3.50,
+        descripcion: "Pin de Bob Esponja en su expresión característica, perfecto para los fans de Fondo de Bikini."
+    },
+    {
+        id: "PIN-11",
+        titulo: "Pin de Nintendo",
+        imagen: "./IMG/nintendo.jpg",
+        imagenesCarrusel: ["./IMG/nintendo.jpg", "./IMG/nintendomano.jpg"],
+        categoria: { nombre: "Videojuegos", id: "videojuegos" },
+        precio: 3.50,
+        descripcion: "Pin clásico de Nintendo, con un diseño retro que evoca la nostalgia de los primeros juegos."
+    },
+    {
+        id: "PIN-12",
+        titulo: "Pin de Ganyu",
+        imagen: "./IMG/genshi.jpg",
+        imagenesCarrusel: ["./IMG/genshi.jpg", "./IMG/genshi1.jpg"],
+        categoria: { nombre: "Genshin Impact", id: "Genshin Impact" },
+        precio: 7.50,
+        descripcion: "Pin de Ganyu, la arquera de hielo de Genshin Impact, en una pose elegante y detallada."
+    },
+    {
+        id: "PIN-13",
+        titulo: "Pin de Xiao",
+        imagen: "./IMG/genshin.jpg",
+        imagenesCarrusel: ["./IMG/genshin.jpg", "./IMG/genshin2.jpg"],
+        categoria: { nombre: "Genshin Impact", id: "Genshin Impact" },
+        precio: 6.50,
+        descripcion: "Pin de Xiao, el guardián de Liyue en Genshin Impact, con un diseño oscuro y misterioso."
+    },
+    {
+        id: "PIN-14",
+        titulo: "Pin de Astronauta Estrella",
+        imagen: "./IMG/starastro.jpg",
+        imagenesCarrusel: ["./IMG/starastro.jpg", "./IMG/starastro1.jpg"],
+        categoria: { nombre: "Espacio", id: "espacio" },
+        precio: 3.50,
+        descripcion: "Pin de un astronauta con una estrella en el fondo, perfecto para los amantes del cosmos."
+    },
+    {
+        id: "PIN-15",
+        titulo: "Pin de Capibara Cumpleañero",
+        imagen: "./IMG/capibaracum.jpg",
+        imagenesCarrusel: ["./IMG/capibaracum.jpg", "./IMG/capibaracum1.jpg"],
+        categoria: { nombre: "Animales", id: "animales" },
+        precio: 3.50,
+        descripcion: "Pin de una capibara celebrando su cumpleaños, con un gorrito festivo y una expresión feliz."
+    },
+    {
+        id: "PIN-17",
+        titulo: "Pin de Gatito en Maceta",
+        imagen: "./IMG/Gatomaceta.jpg",
+        imagenesCarrusel: ["./IMG/Gatomaceta.jpg", "./IMG/Gatomaceta1.jpg"],
+        categoria: { nombre: "Gatos", id: "Gatos" },
+        precio: 3.00,
+        descripcion: "Pin de un gatito asomándose desde una maceta, una combinación encantadora de naturaleza y felinos."
+    },
+    {
+        id: "PIN-18",
+        titulo: "Pin de Fantasma Espeluznante",
+        imagen: "./IMG/fantasmabook.jpg",
+        imagenesCarrusel: ["./IMG/fantasmabook.jpg", "./IMG/fantasmabook1.jpg"],
+        categoria: { nombre: "Horror", id: "horror" },
+        precio: 3.50,
+        descripcion: "Pin de un fantasma espeluznante saliendo de un libro, ideal para los fans de lo paranormal."
+    },
+    {
         id: "PIN-19",
-        titulo: "Pin de Gato",
+        titulo: "Pin de Gato con Flores",
         imagen: "./IMG/gatoflorespin.jpg",
         imagenesCarrusel: ["./IMG/gatoflorespin.jpg", "./IMG/gatofloresmano.jpg"],
-        categoria: {
-            nombre: "Pokemon",
-            id: "pokemon"
-        },
+        categoria: { nombre: "Gatos", id: "Gatos" },
         precio: 3.50,
-        descripcion: "Pin de gato adorable."
-        } */
-  ];
+        descripcion: "Pin de un gato adornado con flores, una combinación de naturaleza y felinos para los amantes del arte."
+    }
+];
 
   const contenedorProductos = document.querySelector("#contenedor_productos");
   const popup = document.querySelector("#product-popup");
